e3ce9d4c0b8937440b589400ca61b093ab87cb8e
Júlia Ferreira <145815456+jujwd@users.noreply.github.com>
2024-03-08 22:28:26 -0300

    Add files via upload

---
+++ 1.0.3.2.js
@@ -0,0 +1,12 @@
+const spotify = {
+    "nome": "Júlia Dias Ferreira",
+    "e-mail": "contato.juliad@gmail.com",
+    "plano": "Plano Estudantil",
+    "utilizaSpotify": true,
+    "itensCompra": [
+        "EcoBag",
+        "Disco de Vinil",
+        "Camiseta Personalizada",
+        "Copo de Café Personalizado"
+    ]
+}
